ffc162a669e34d8683e30e09ba258952b0314630
Zakhar <cloud9CPP@yandex.ru>
2020-03-08 15:58:57 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let money = +prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: '',
+    income : [],
+    savings: ''
+};
+
+checkSallary();
+function checkSallary() {
+  for (let i = 0; i < 1; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+      b = +prompt("Во сколько обойдется?");
+ 
+    if ((typeof (a)) === 'string' && (typeof (b)) === 'number' && (typeof (a) != null) && (typeof (b) != null && a != '' && b != '' && a.length < 50)) {
+      console.log('done');
+      appData.expenses[a] = b;
+    } else {
+      return checkSallary();
+    }
+  }
+}
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Бюджет на один день равен ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay <= 100){
+    console.log("Минимальный уровень достатка");
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay >= 2000) {
+    console.log("Высокий уровень достатка");
+} else{
+    console.log("Ошибка");
+}
